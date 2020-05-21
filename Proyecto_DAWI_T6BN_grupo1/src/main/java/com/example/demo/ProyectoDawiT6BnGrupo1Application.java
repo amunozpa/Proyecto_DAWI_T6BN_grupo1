09e08da340c83f183d9423664053b65daeb6730b
unknown <amueedinson@gmail.com>
2020-05-20 22:08:26 -0500

    primer commit

---
+++ Proyecto_DAWI_T6BN_grupo1/src/main/java/com/example/demo/ProyectoDawiT6BnGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawiT6BnGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawiT6BnGrupo1Application.class, args);
+	}
+
+}
